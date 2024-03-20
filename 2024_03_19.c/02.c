6cd681e3531a9ef5e293cd496db87b8353bafcfb
azurehanzz <jiangjinghan008@gmail.com>
2024-03-20 09:46:35 +0800

    2024_03_19.c

---
+++ 2024_03_19.c/02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	char arr[] = { 'a','b','c','d','e','f' };
+	printf("%d\n", sizeof(arr));
+	printf("%d\n", sizeof(arr + 0));
+	printf("%d\n", sizeof(*arr));
+	printf("%d\n", sizeof(arr[1]));
+	printf("%d\n", sizeof(&arr));
+	printf("%d\n", sizeof(&arr + 1));
+	printf("%d\n", sizeof(&arr[0] + 1));
+	printf("%d\n", strlen(arr));
+	printf("%d\n", strlen(arr + 0));
+	//printf("%d\n", strlen(*arr));
+	//printf("%d\n", strlen(arr[1]));
+	printf("%d\n", strlen(&arr));
+	printf("%d\n", strlen(&arr + 1));
+	printf("%d\n", strlen(&arr[0] + 1));
+}
